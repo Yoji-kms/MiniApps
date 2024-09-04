797fe1b789f25b77c1628ad716e59741f6b7966e
Mashnin Egor <mashnin_yegor@mail.ru>
2024-09-04 13:48:20 +0300

    Initial Commit

---
+++ MiniAppsTests/MiniAppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAppsTests.swift
+//  MiniAppsTests
+//
+//  Created by Yoji on 04.09.2024.
+//
+
+import Testing
+@testable import MiniApps
+
+struct MiniAppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
